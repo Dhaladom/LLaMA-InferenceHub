36ee38adf3e96a095d2cb30a98677645ee8f3f90
dhaladom <dhaladom@posteo.de>
2023-05-11 16:28:22 +0000

    preparing the repo

--- llama_inference.py
+++ llama_inference.py
@@ -1,27 +1,16 @@
-import argparse
-
 import torch
-import torch.nn as nn
 import quant
 
-from gptq import GPTQ
-from utils import find_layers, DEV, set_seed, get_wikitext2, get_ptb, get_c4, get_ptb_new, get_c4_new, get_loaders
+from utils import find_layers, DEV
 import transformers
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, TextStreamer, GenerationConfig
+import accelerate
+from time import time
 
+from os import environ
 
-def get_llama(model):
-
-    def skip(*args, **kwargs):
-        pass
-
-    torch.nn.init.kaiming_uniform_ = skip
-    torch.nn.init.uniform_ = skip
-    torch.nn.init.normal_ = skip
-    from transformers import LlamaForCausalLM
-    model = LlamaForCausalLM.from_pretrained(model, torch_dtype='auto')
-    model.seqlen = 2048
-    return model
+import llama_accelerate_path
+import json 
 
 
 def load_quant(model, checkpoint, wbits, groupsize=-1, fused_mlp=True, eval=True, warmup_autotune=True):
@@ -53,7 +42,7 @@ def load_quant(model, checkpoint, wbits, groupsize=-1, fused_mlp=True, eval=True
     print('Loading model ...')
     if checkpoint.endswith('.safetensors'):
         from safetensors.torch import load_file as safe_load
-        model.load_state_dict(safe_load(checkpoint), strict=False)
+        model.load_state_dict(safe_load(checkpoint), strict=True)
     else:
         model.load_state_dict(torch.load(checkpoint), strict=False)
 
@@ -69,55 +58,54 @@ def load_quant(model, checkpoint, wbits, groupsize=-1, fused_mlp=True, eval=True
     model.seqlen = 2048
     print('Done.')
 
+
+    device_map = json.load(open("/home/ddl/LLaMA-InferenceHub/config/device_map.json", 'r'))
+    model = accelerate.dispatch_model(model, device_map=device_map)     
+    
+    # model = llama_accelerate_path.apply_to_model(model)
+
+
+    print('Done.')
+
     return model
 
+def inference(model, tokenizer, input, gen_config, streamer=None):    
 
-if __name__ == '__main__':
+    input_ids = tokenizer.encode(input, return_tensors="pt").to(torch.device('cuda:1'))
 
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('model', type=str, help='llama model to load')
-    parser.add_argument('--wbits', type=int, default=16, choices=[2, 3, 4, 8, 16], help='#bits to use for quantization; use 16 for evaluating base model.')
-    parser.add_argument('--groupsize', type=int, default=-1, help='Groupsize to use for quantization; default uses full row.')
-    parser.add_argument('--load', type=str, default='', help='Load quantized model.')
-
-    parser.add_argument('--text', type=str, help='input text')
-
-    parser.add_argument('--min_length', type=int, default=10, help='The minimum length of the sequence to be generated.')
-
-    parser.add_argument('--max_length', type=int, default=50, help='The maximum length of the sequence to be generated.')
-
-    parser.add_argument('--top_p',
-                        type=float,
-                        default=0.95,
-                        help='If set to float < 1, only the smallest set of most probable tokens with probabilities that add up to top_p or higher are kept for generation.')
-
-    parser.add_argument('--temperature', type=float, default=0.8, help='The value used to module the next token probabilities.')
-
-    parser.add_argument('--device', type=int, default=-1, help='The device used to load the model when using safetensors. Default device is "cpu" or specify, 0,1,2,3,... for GPU device.')
-
-    args = parser.parse_args()
-
-    if type(args.load) is not str:
-        args.load = args.load.as_posix()
-
-    if args.load:
-        model = load_quant(args.model, args.load, args.wbits, args.groupsize)
-    else:
-        model = get_llama(args.model)
-        model.eval()
-
-    model.to(DEV)
-    tokenizer = AutoTokenizer.from_pretrained(args.model, use_fast=False)
-    input_ids = tokenizer.encode(args.text, return_tensors="pt").to(DEV)
-
+    generation_config = gen_config
+    time1 = time()
     with torch.no_grad():
         generated_ids = model.generate(
             input_ids,
-            do_sample=True,
-            min_length=args.min_length,
-            max_length=args.max_length,
-            top_p=args.top_p,
-            temperature=args.temperature,
+            #streamer=streamer,
+           **generation_config
         )
-    print(tokenizer.decode([el.item() for el in generated_ids[0]]))
+
+    token_count = len(generated_ids[0]) - len(input_ids[0])
+
+    output = tokenizer.decode([el.item() for el in generated_ids[0]])
+    print(f"Tokens per second: {token_count / (time() - time1)}")
+
+    return output
+
+if __name__ == '__main__':
+
+    model_dir: str = "/home/ddl/models/Neko-Institute-of-Science_LLaMA-65B-4bit-128g"
+    model_file_path: str = "/home/ddl/models/Neko-Institute-of-Science_LLaMA-65B-4bit-128g/gptq_model-4bit-128g.safetensors"
+    wbits: int = 4
+    groupsize: int = 128
+
+    model:str = load_quant(model_dir, model_file_path, wbits, groupsize, eval=True, warmup_autotune=True)
+    tokenizer = AutoTokenizer.from_pretrained(model_dir, use_fast=False)
+    streamer = TextStreamer(tokenizer)
+    gen_config = GenerationConfig._dict_from_json_file("/home/ddl/LLaMA-InferenceHub/config/gen_default.json")
+
+    while True:
+
+        prompt = "Hello, my name is"
+        output = inference(model, tokenizer, prompt, gen_config, streamer)
+        print(output)
+
+
+
